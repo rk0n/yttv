a132e744c1f9a19e33dbd5e602cde3117a685512
rk0n <rk0n@gmx.de>
2022-04-14 10:30:58 +0200

    Refactor to library

--- README.md
+++ README.md
@@ -2,15 +2,40 @@
 Python API for YouTubeTV (https://youtube.com/tv)
 
 ## Installation
+```bash
 poetry install
 wget https://github.com/gorhill/uBlock/releases/download/1.42.4/uBlock0_1.42.4.firefox.signed.xpi
 poetry run python yttv.py
+```
+
+## Testing
+```bash
+poetry run pytest
+```
+
+## docker
+### docker-compose
+```bash
+docker-compose up -d
+```
 
 ## Todo
+### 0.1.X
 - [X] Get YouTube TV Page
 - [X] Go to Settings
 - [X] Get TV Code
 - [X] Add script block
-- [ ] Get multiple TV Code
-- [ ] Persistence
+- [X] Get multiple TV Code
+- [X] Persistence
+- [ ] Dockerize
+- [ ] Add automated XPI Download
+- [ ] Add Tests
+- [ ] Add Docs
+- [ ] Add Versioning
 - [ ] Make python package
+### 0.2.X
+- [ ] Enhance crawling template creation (Maybe use accesible_name) 
+- [ ] Enhance speed (check if site is fully rendered)
+- [ ] Enhance dockerfile (check if site is fully rendered)
+### 0.3.X
+- [ ] Think about new options and add them
--- yttv.py
+++ yttv.py
@@ -1,4 +1,7 @@
+import os
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import time
@@ -6,56 +9,91 @@ import time
 USER_AGENT = 'Mozilla/5.0 (SMART-TV; Linux; Tizen 4.0.0.2) AppleWebkit/605.1.15 (KHTML, like Gecko) SamsungBrowser/9.2 TV Safari/605.1.15'
 SCRIPT_BLOCKER = 'uBlock0_1.42.4.firefox.signed.xpi'
 
+
+class YTTV:
+
+    def __init__(self):
+        PROFILE_DIRECTORY = '.yttv_config/firefox_profile'
+        os.makedirs(PROFILE_DIRECTORY, exist_ok=True)
+        # Configure Browser
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        profile = webdriver.FirefoxProfile(profile_directory=PROFILE_DIRECTORY)
+        profile.set_preference("general.useragent.override", USER_AGENT)
+
+        self.driver = webdriver.Firefox(options=options,
+                                        firefox_profile=profile)
+        self.driver.delete_all_cookies()
+        self.driver.install_addon(SCRIPT_BLOCKER, temporary=True)
+
+    def stop_app(self):
+        self.driver.quit()
+
+    def get_settings_tv_code(self) -> str:
+        # Go to Settings
+        button = self.driver.find_element(By.CLASS_NAME, 'ytlr-icon--gear')
+        print("Go to Settings")
+        button.click()
+        for idx in range(5):
+            print(idx)
+            time.sleep(1)
+        print("Settings")
+        # Go to Link with TV Code
+        buttons = self.driver.find_elements(By.CLASS_NAME,
+                                            'yt-virtual-list__item')
+        for button in buttons:
+            if button.text == 'Link with TV code':
+                print("Go to Link with TV Code")
+                button.click()
+                for idx in range(5):
+                    print(idx)
+                    time.sleep(1)
+                print("Link with Wi-Fi")
+        # Get TV Code
+        print("Get TV Code")
+        code = self.driver.find_element(By.CLASS_NAME,
+                                        'ytlr-link-phone-with-tv-code-renderer__pairing-code-text')
+        print(code.text)
+        return code.text.replace(' ', '')
+
+    def load_app(self):
+        # Loading
+        self.driver.get('https://www.youtube.com/tv')
+        print("LOADING..")
+        for i in range(5):
+            print(i)
+            time.sleep(1)
+
+    def reset_app(self):
+        print("RESET..")
+        running = True
+        i = 0
+        while running:
+            webdriver.ActionChains(self.driver).send_keys(
+                Keys.ESCAPE).perform()
+            for z in range(5):
+                print(z)
+                time.sleep(z)
+            elements = self.driver.find_elements(By.TAG_NAME,
+                                                 'ytlr-button-renderer')
+            print(i)
+            i = + 1
+            if len(elements) == 2:
+                for element in elements:
+                    if element.text == 'CANCEL':
+                        running = False
+                        element.click()
+                        time.sleep(1)
+                        break
+
+
 if __name__ == '__main__':
-    # Configure Browser
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    # Configure Profile
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("general.useragent.override", USER_AGENT)
-
-    driver = webdriver.Firefox(options=options, firefox_profile=profile)
-    driver.install_addon(SCRIPT_BLOCKER, temporary=True)
-
-    # Loading
-    driver.get('https://www.youtube.com/tv#/')
-    print("LOADING..")
-    for i in range(3):
-        print(i)
-        time.sleep(1)
-
-    # Go to Settings
-    button = driver.find_element(By.CLASS_NAME, 'ytlr-icon--gear')
-    print("Go to Settings")
-    button.click()
-    for i in range(2):
-        print(i)
-        time.sleep(1)
-    print("Settings")
-
-    # Go to Link with TV Code
-    buttons = driver.find_elements(By.CLASS_NAME, 'yt-virtual-list__item')
-    for button in buttons:
-        if button.text == 'Link with TV code':
-            print("Go to Link with TV Code")
-            button.click()
-            for i in range(2):
-                print(i)
-                time.sleep(1)
-            print("Link with Wi-Fi")
-
-    # Get TV Code
-    print("Get TV Code")
-    code = driver.find_element(By.CLASS_NAME, 'ytlr-link-phone-with-tv-code-renderer__pairing-code-text')
-    print(code.text)
-
-    # Screenshot
-    print("Screenshot")
-    webpage = driver.find_element(By.TAG_NAME, "body")
-    webpage.screenshot('page.png')
-
-    print('STARTED..')
-    while True:
-        time.sleep(1)
-    # Postprocess
-    # driver.quit()
+    yttv = YTTV()
+    try:
+        yttv.load_app()
+        tv_code = yttv.get_settings_tv_code()
+        time.sleep(60)
+    except Exception as e:
+        print(e)
+        yttv.stop_app()
+    yttv.stop_app()
